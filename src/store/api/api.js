cdd0a5bdf3ecc982052a1311573882002e09494f
hefulin <hefulin8023@163.com>
2018-05-19 11:10:20 +0800

    新闻

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -5,7 +5,16 @@ import * as Url from "../url/url"
  //后台接口地址
  // const BASE_URL = 'http://101.230.199.221:7005/wrmsApp'
  const BASE_URL = 'http://localhost:8080'
-
+ /**
+  * @name 取水相关新闻列表
+  * @param
+  * */
+ export const NEWS_LIST = BASE_URL + '/app/waternews/getWaterNewsList?access_token=[b36680850768ff1b]';
+/**
+ * @name 取水相关新闻详细内容
+ * @param infoid
+ * */
+ export const NEWS_CONTENT = BASE_URL + '/app/waternews/getWaterNewsInfo?access_token=[b36680850768ff1b]';
  /**取水户 都以qsh 开头*/
  /**
   * @name 取水户列表
